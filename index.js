baef8c155a7a03ba97c61f8c96fe6c87b1ad9298
mdevolder <matthew.devolder@gmail.com>
2021-09-20 13:13:42 -0500

    Basic site setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const ejsMate = require('ejs-mate')
+
+const app = express()
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
+
+app.listen(3000, () => {
+  console.log('Serving on port 3000')
+})
